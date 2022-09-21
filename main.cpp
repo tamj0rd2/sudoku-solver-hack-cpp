0f8ef63124c67fdd03a35ece1781ec6a853baea5
Tamara Jordan <tamara.jordan@saltpay.co>
2022-09-21 15:09:22 +0100

    Find the puzzle outline blob

--- main.cpp
+++ main.cpp
@@ -1,31 +1,77 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+using namespace cv;
+
 
 int main(int argc, char** argv) {
+    Mat sudoku = imread(SUDOKU_IMG_PATH, 0);
+    Mat outerBox = Mat(sudoku.size(), CV_8UC1);
 
-    //create a gui window:
-    cv::namedWindow("Output",1);
-    cv::moveWindow("output", -1000, 0);
+    // blur the image to reduce noise from paper texture
+    GaussianBlur(sudoku, sudoku, Size(11,11), 0);
 
-    //initialize a 120X350 matrix of black pixels:
-    cv::Mat output = cv::Mat::zeros( 120, 350, CV_8UC3 );
+    // threshold the image to capture the outerbox which we care about - too much going on to do the rest atm
+    adaptiveThreshold(sudoku, outerBox, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 2);
 
-    //write text on the matrix:
-    cv::putText(output,
-            "Hello World :)",
-            cv::Point(15,70),
-            cv::FONT_HERSHEY_PLAIN,
-            3,
-            cv::Scalar(0,255,0),
-            4);
+    // flip the bits so that the stuff we want to select (the lines) are white
+    bitwise_not(outerBox, outerBox);
 
-    //display the image:
-    cv::imshow("Output", output);
+    // fill in any gaps that have formed
+    Mat kernel = (Mat_<uchar>(3,3) << 0,1,0,1,1,1,0,1,0);
+    dilate(outerBox, outerBox, kernel);
 
-    //wait for the user to press any key:
-    cv::waitKey(0);
 
-    return 0;
 
+    // find the biggest blob
+    int count=0;
+    int max=-1;
+
+    Point maxPt;
+
+    for(int y=0;y<outerBox.size().height;y++)
+    {
+        uchar *row = outerBox.ptr(y);
+        for(int x=0;x<outerBox.size().width;x++)
+        {
+            // in practice, this can be anything other than 1
+            if(row[x]>=128)
+            {
+
+                int area = floodFill(outerBox, Point(x,y), CV_RGB(0,0,64));
+
+                if(area>max)
+                {
+                    maxPt = Point(x,y);
+                    max = area;
+                }
+            }
+        }
+    }
+    floodFill(outerBox, maxPt, CV_RGB(255,255,255));
+
+    // fill the other non-write pixels with black to hide them
+    for(int y=0;y<outerBox.size().height;y++)
+    {
+        uchar *row = outerBox.ptr(y);
+        for(int x=0;x<outerBox.size().width;x++)
+        {
+            if(row[x]==64 && x!=maxPt.x && y!=maxPt.y)
+            {
+                int area = floodFill(outerBox, Point(x,y), CV_RGB(0,0,0));
+            }
+        }
+    }
+
+    // show stuff
+    const char *origWindowName = "Input";
+    namedWindow(origWindowName, 1);
+    imshow(origWindowName, sudoku);
+
+    const char *outputWindownName = "Outer box";
+    namedWindow(outputWindownName, 1);
+    moveWindow(outputWindownName, 0, 500);
+    imshow(outputWindownName, outerBox);
+
+    waitKey(0);
 }
